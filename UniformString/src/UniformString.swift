6d84a86debcd5e6a25489c07c4e265715f50e834
Henry <rabbit.henry@gmail.com>
2015-02-12 16:31:38 +0800

    Generate general string control methods. length, search, replace, split.

--- UniformString/src/UniformString.swift
+++ UniformString/src/UniformString.swift
@@ -10,4 +10,32 @@ import Foundation
 
 extension String {
     
+    public func length() -> Int! {
+        return count(self)
+    }
+    
+    public func search(str : String) -> Range<String.Index>? {
+        return self.rangeOfString(str)
+    }
+    
+    public func splitBy(character : Character) -> [String]? {
+        return split(self, {$0==character}, maxSplit: Int.max, allowEmptySlices: false)
+    }
+    
+    public func replace(string : String, withString : String) -> String {
+        return self.stringByReplacingOccurrencesOfString(string, withString: withString, options: .LiteralSearch, range: nil)
+    }
+    
+    public func trim() -> String {
+        return self.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+    }
+    
+    subscript (r: Range<Int>) -> String {
+        get {
+            let startIndex = advance(self.startIndex, r.startIndex)
+            let endIndex = advance(startIndex, r.endIndex - r.startIndex)
+            
+            return self[Range(start: startIndex, end: endIndex)]
+        }
+    }
 }
