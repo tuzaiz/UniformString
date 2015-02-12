3fad86c2f41062d307b549e82f16a10a82b86c03
Henry <rabbit.henry@gmail.com>
2015-02-12 16:15:00 +0800

    Generate Xcode project.

---
+++ UniformString/src/UniformString.swift
@@ -0,0 +1,13 @@
+//
+//  UniformString.swift
+//  UniformString
+//
+//  Created by Henry Tseng on 2015/2/12.
+//  Copyright (c) 2015年 Cloudbay. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    
+}
